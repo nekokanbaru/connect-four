3c8962a0fa142672eff5c03af06c45060f70f8db
nekokanbaru <mihailorang@gmail.com>
2023-01-18 21:49:30 +0100

    add functionality

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ function place(columnID) {
   let columnRows = column.children;
   let rowIndex = null;
   let circle = document.querySelector("#circle");
+  let currentPlayerCircle = document.querySelector('.chip');
+  let currentPlayerText = document.querySelector('#playerText');
 
   for (let i = columnRows.length - 1; i >= 0; i--) {
     if (
@@ -27,13 +29,21 @@ function place(columnID) {
     }
   }
   if (this.currentPlayer == 1) {
+    currentPlayerText.innerText = "Player 1";
     columnRows[rowIndex].style.backgroundColor = "yellow";
     circle.style.backgroundColor = "red";
+    currentPlayerCircle.style.backgroundColor ="red";
+    currentPlayerCircle.style.boxShadow = "inset 5px 0px 5px rgb(128, 0, 0)";
   } else {
+    currentPlayerText.innerText = "Player 2";
     columnRows[rowIndex].style.backgroundColor = "red";
     circle.style.backgroundColor = "yellow";
+    currentPlayerCircle.style.backgroundColor ="yellow";
+    currentPlayerCircle.style.boxShadow = "inset 5px 0px 5px rgb(145, 145, 7)";
   }
+
   this.currentPlayer == 1 ? (this.currentPlayer = 2) : (this.currentPlayer = 1);
+  
 }
 
 function isGameFinished(currentColumn, currentRow) {
@@ -43,19 +53,90 @@ function isGameFinished(currentColumn, currentRow) {
     columnArray.push(document.querySelector("#column" + i));
   }
 
-  //left right check
-  if (currentColumnIndex != 0 && currentColumnIndex != 6) {
-    count = 1;
-    //check left first
-    while (isPoint(columnArray[currentColumnIndex - 1].children[currentRow])) {
-      count++;
+  const initialColumnIndex = currentColumnIndex;
+  const initialRow = currentRow;
+ 
+  let count = 1; //count starts at 1 because there is always one chip that we just inserted
+    
+    //first check right and left
+    while(currentColumnIndex != 6  && isPoint(columnArray[currentColumnIndex + 1].children[currentRow])) {
+      count ++;
       currentColumnIndex++;
-      console.log(count);
     }
-    if (count == 4) {
-      console.log("u win retard");
-    }
-  }
+      currentColumnIndex = initialColumnIndex; //start at the initial chip location and check left
+      while(currentColumnIndex != 0 && isPoint(columnArray[currentColumnIndex - 1].children[currentRow])) {
+        currentColumnIndex --;
+        count ++;
+      }
+      if(count >= 4) {
+       playerWins()
+      }
+      //if there aren't 4 connected right and left, we check if there are 4 connected downwards
+      //we don't check up because it isn't possible to insert a circle on the bottom
+      else {
+        //reset count and current column, because we start the count over once we change directions
+        currentColumnIndex = initialColumnIndex;
+        count = 1;
+        while(currentRow != 5 && isPoint(columnArray[currentColumnIndex].children[currentRow + 1])){
+          currentRow ++;
+          count ++;
+        }
+        if(count >= 4) {
+          playerWins()
+        }
+        //if there aren't 4 connected downwards, we check if there are 4 connected right diagonally
+        else {
+          currentColumnIndex = initialColumnIndex;
+          currentRow = initialRow;
+          count = 1;
+          //first start right diagonally up
+          while(currentRow != 0 && currentColumnIndex != 6 &&
+            isPoint(columnArray[currentColumnIndex+1].children[currentRow-1]))
+            {
+              count ++;
+              currentColumnIndex ++;
+              currentRow --;
+            }
+          //start from the initial position and check right diagonally down
+          currentColumnIndex = initialColumnIndex;
+          currentRow = initialRow;
+          while(currentRow !=5 && currentColumnIndex != 0 &&
+            isPoint(columnArray[currentColumnIndex-1].children[currentRow+1])){
+              count++;
+              currentColumnIndex--;
+              currentRow++;
+            }
+            if(count >= 4) {
+              playerWins()
+            }
+            //if there aren't 4 connected right diagonally, we check if there are 4 connected left diagonally
+            else {
+             currentColumnIndex = initialColumnIndex;
+             currentRow = initialRow;
+             count = 1;
+             //first start left diagonally up
+             while(currentRow != 0 && currentColumnIndex != 0 &&
+              isPoint(columnArray[currentColumnIndex-1].children[currentRow-1])) {
+                count ++;
+                currentColumnIndex --;
+                currentRow --;
+             }
+             //start from the initial position and check left diagonally down
+             currentColumnIndex = initialColumnIndex;
+             currentRow = initialRow;
+
+             while(currentRow != 5 && currentColumnIndex != 6 &&
+              isPoint(columnArray[currentColumnIndex+1].children[currentRow+1])) {
+                count ++;
+                currentColumnIndex ++;
+                currentRow ++;
+             }
+             if(count >= 4) {
+              playerWins()
+            }
+        }
+      }
+}
 }
 
 // red = rgb(255, 0, 0) has to have spaces
@@ -72,12 +153,42 @@ function isPoint(circle) {
   }
 }
 
-// function isYellow(circle) {
-//   return window.getComputedStyle(circle).backgroundColor == "rgb(255, 255, 0)";
-// }
+function playerWins() {
+  let wrapper = document.querySelector(".board-wrapper");
+  wrapper.style.display = 'none';
 
-// function isEmpty(circle) {
-//   return (
-//     window.getComputedStyle(circle).backgroundColor == "rgb(255, 255, 255)"
-//   );
-// }
+  let winScreen = document.querySelector('.win-screen');
+  winScreen.style.display = 'flex';
+  let winScreenPlayer = document.querySelector("#win-screen-player");
+  if(this.currentPlayer == 1){
+    winScreenPlayer.style.color = 'yellow';
+    winScreenPlayer.innerText = 'Player 2'
+  }
+  else {
+    winScreenPlayer.style.color = 'red';
+    winScreenPlayer.innerText = 'Player 1';
+  }
+}
+
+function playAgain() {
+  resetTable()
+  let wrapper = document.querySelector(".board-wrapper");
+  wrapper.style.display = 'block';
+
+  let winScreen = document.querySelector('.win-screen');
+  winScreen.style.display = 'none';
+}
+
+function resetTable() {
+  this.currentPlayer == 1 ? (this.currentPlayer = 2) : (this.currentPlayer = 1);
+  let columnArray = [];
+  for (let i = 1; i <= 7; i++) {
+    columnArray.push(document.querySelector("#column" + i));
+  }
+  for(let i = 0; i<7; i++) {
+    for(let j = 0; j<6; j++) {
+      columnArray[i].children[j].style.backgroundColor = 'white';
+    }
+  }
+  console.log(columnArray);
+}
