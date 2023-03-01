2ae345fcd67c1cdff5e1f441f45ddca91d5385be
nekokanbaru <mihailorang@gmail.com>
2023-03-01 13:30:51 +0100

    change require ws to websocket

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // const http = require("http");
-const WebSocket = require('ws');
+const WebSocket = require('websocket');
 const app = require("express")();
 app.get("/", (req,res) => res.sendFile(__dirname + "/index.html"));
 app.get("/style.css", (req,res) => res.sendFile(__dirname + "/style.css"));
